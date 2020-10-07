52f1569900241a1ae9b07a934cbbbc66bd1611f5
Marzeu <marzahell@hotmail.com>
2020-10-06 23:07:22 -0300

    rotas produtos, clientes e usuarios criada

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);

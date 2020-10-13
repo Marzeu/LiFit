cd6feb687514ee9b7380656f472fed45cd553d57
Marzeu <marzahell@hotmail.com>
2020-10-13 20:29:21 -0300

    cadastro de novos produtos finalizado

--- public/produtos/novo/main.js
+++ public/produtos/novo/main.js
@@ -11,19 +11,19 @@ input.addEventListener("keydown", function (e) {
         value = new Intl.NumberFormat('pt-BR', {
             style: 'currency',
             currency: 'BRL'
-        }).format(value / 100)
+        }).format(value / 100);
 
         e.target.value = value;
     }, 1);
-})
+});
 
 // dentro do replace é passado o parametro de expressões regulares que irá substituir, 
 // nesse caso tudo o que não for número será substituido por um campo vazio "".
 // O construtor formata para o R$ padrão internacional.
 
 const URL = window.location.hostname.includes('localhost')
-   ? 'http://localhost:8080/products'
-     : 'https://api-lifit.herokuapp.com/products';
+    ? 'http://localhost:8080/products'
+    : 'https://api-lifit.herokuapp.com/products';
 
 async function postProducts() {
 
@@ -33,32 +33,43 @@ async function postProducts() {
     const quantity = document.getElementById('quantity').value;
     const url = document.getElementById('url').value;
 
-    const body = { name, description, price, quantity, url }
-try {
-    const res = await fetch("/products", {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    });
-    if (res.status != 201 ) {
-        swal("Good job!", "You clicked the button!", "error");
-        return;
+    const body = { name, description, price, quantity, url };
+    try {
+        const res = await fetch(URL, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        });
+        if (res.status != 201) {
+            swal("Não deu.", "Algo de errado aconteceu", "error");
+            return;
+        };
+        const data = await res.json();
+        sweetAlert();        
     }
-    const data = await res.json();
-    swal("Good job!", "You clicked the button!", "success");
-    console.log(data);    
-}
-catch {
-    swal("Good job!", "You clicked the button!", "error");
-}
-
-}
-// console.log(res)
-// window.location.href = '/'
+    catch {
+        swal("Não deu.", "Algo de errado aconteceu", "error");
+    };
+};
 
 function sweetAlert() {
-    swal("Good job!", "You clicked the button!", "success");
+    swal({
+        title: "Produto cadastrado com sucesso!",
+        text: "Deseja cadastrar outro produto?",
+        icon: "success",        
+        buttons: {
+            cancel: true,
+            confirm: true,
+        },
+    })
+    .then((cadastrarNovo) => {
+        if (cadastrarNovo) {
+            window.location.href = '/produtos/novo'
+        } else {
+            window.location.href = '/'
+        };
+    });
 };

d31d5d0c3d2ac1e222f0c4570fa03f4f2028b999
Marzeu <marzahell@hotmail.com>
2020-10-26 20:28:33 -0300

    refatoração

--- public/usuarios/produtos/novo/main.js
+++ public/usuarios/produtos/novo/main.js
@@ -1,26 +1,3 @@
-// máscara que converte o valor em no campo price do tipo text em dinheiro 
-
-const moneyMask = document.querySelector('input[name="price"]');
-moneyMask.addEventListener("keydown", function (e) {
-
-    setTimeout(function () {
-        let value = e.target.value;
-
-        value = value.replace(/\D/g, "");
-
-        value = new Intl.NumberFormat('pt-BR', {
-            style: 'currency',
-            currency: 'BRL'
-        }).format(value / 100);
-
-        e.target.value = value;
-    }, 1);
-});
-
-// dentro do replace é passado o parametro de expressões regulares que irá substituir, 
-// nesse caso tudo o que não for número será substituido por um campo vazio "".
-// O construtor formata para o R$ padrão internacional.
-
 const URL = window.location.hostname.includes('localhost')
     ? 'http://localhost:8080/products'
     : 'https://api-lifit.herokuapp.com/products';

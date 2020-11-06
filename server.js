32cccd8439f081bf3fd265099fde2598ed7fca2a
Marzeu <marzahell@hotmail.com>
2020-11-05 21:34:52 -0300

    limpeza

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const jsonServer = require('json-server');
 const path = require('path');
-// const bodyParser = require('body-parser');
 
 const router = jsonServer.router(path.join(__dirname, './database/db.json'))
 const server = jsonServer.create();
@@ -11,15 +10,6 @@ const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
-
-// server.use(jsonServer.bodyParser)
-// server.use((req, res, next) => {
-//   if (req.method === 'POST') {
-//     req.body.createdAt = Date.now()
-//   }
-//   // Continue to JSON Server router
-//   next()
-// })
 
 server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
